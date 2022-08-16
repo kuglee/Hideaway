cf029b2117431d8e2911edbec7c4615b689b8b53
Gábor Librecz <kuglee@gmail.com>
2022-08-16 21:47:39 +0200

    Initial Commit

---
+++ Hideaway/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Hideaway
+//
+//  Created by Librecz Gábor on 2022. 08. 16..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hideaway/HideawayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HideawayApp.swift
+//  Hideaway
+//
+//  Created by Librecz Gábor on 2022. 08. 16..
+//
+
+import SwiftUI
+
+@main
+struct HideawayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
